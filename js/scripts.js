c1751808f93feb2c58721c6b315410825f73f908
DavidCiccone <davidkciccone@gmail.com>
2017-06-25 14:27:05 -0400

    fade

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 function displayQuote(){
-	$( ".quote" ).empty();
+	$( ".quote" ).fadeOut("fast");
 	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
 	  $(".quote").replaceWith('<div class="quote"><p class="quo">' + a[0].content + '</p>' + '<p>— ' + a[0].title + '</p></div>')
 	});
