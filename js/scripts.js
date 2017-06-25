b6311124d16f01d29a02b03eaa5277bc71509f0c
DavidCiccone <davidkciccone@gmail.com>
2017-06-25 15:31:32 -0400

    twitter update

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,25 @@ function displayQuote(){
 	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
 	  $(".quote").replaceWith('<div class="quote"><p class="quo">' + a[0].content + '</p>' + '<p>— ' + a[0].title + '</p></div>')
 	});
-	
+	/*
 	var quote = document.getElementsByClassName('quo')[0].innerHTML;
 	 $("#twitter").attr("data-text", quote);
 	 console.log(quote);
+	 */
 }
 
 document.getElementById("quoteButton").addEventListener("click", displayQuote);
 
 
 
+twttr.widgets.createShareButton(
+  "https:\/\/dev.twitter.com\/web\/tweet-button",
+  document.getElementById("twitter"),
+  {
+    size: "large",
+    text: "custom share text",
+    hashtags: "example,demo",
+    via: "twitterdev",
+    related: "twitterapi,twitter"
+  }
+);
