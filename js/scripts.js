f6dfa248d42fbe8f3100a7efd55712233843732d
DavidCiccone <davidkciccone@gmail.com>
2017-06-25 14:00:57 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 function displayQuote(){
 	$( ".quo" ).empty();
 	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
-	  $(".quo").replaceWith('<p class="quo">' + a[0].content + '</p>' + '<p class="quo">— ' + a[0].title + '</p>')
+	  $(".quo").replaceWith('<p class="quo">' + a[0].content + '</p>' + '<p>— ' + a[0].title + '</p>')
 	});
 }
 
