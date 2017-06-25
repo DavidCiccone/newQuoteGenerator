1e2e422188b0665ebfef7e61c2224b145380ef1e
DavidCiccone <davidkciccone@gmail.com>
2017-06-25 13:35:55 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+function displayQuote(){
+	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
+	  $(".quote").append(a[0].content + "<p>— " + a[0].title + "</p>")
+	});
+}
+
+document.getElementById("quoteButton").addEventListener("click", displayQuote);
