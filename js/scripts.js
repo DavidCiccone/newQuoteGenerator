37b8ec4e969ecb3c6a2a316f5c5788d9e46a2df2
DavidCiccone <davidkciccone@gmail.com>
2017-06-26 17:03:47 -0400

    Working

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
-
+var counter = 0;
+var storeQuote = document.getElementsByClassName('quote')[0].innerHTML;
+var storeName = document.getElementsByClassName('quote')[0].innerHTML;
+console.log(storeQuote);
 function displayQuote(){
 	
-    var counter = 0;
-	$( ".quote" ).fadeOut("fast");
+    
+	$( ".quo" ).fadeOut("slow");
 	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
 	  $(".quote").replaceWith('<div class="quote"><p class="quo">' + a[0].content + '</p>' + '<p>— ' + a[0].title + '</p></div>')
+      storeQuote = document.querySelector("body > div.wrapper > div > p:nth-child(2)").innerHTML;
+      storeName = document.querySelector("body > div.wrapper > div > p:nth-child(4)").innerHTML;
+      console.log(storeQuote);
+      $('#twitter-widget-' + counter).remove();
+        
+        var newAnchorTag = $('<a></a>')
+            .addClass('twitter-share-button')
+            .attr('href', 'https://twitter.com/intent/tweet')        
+            .attr('data-text', storeQuote + storeName);
+        $('.oneCoolDiv').append(newAnchorTag);
+        //$(".twitter-share-button").attr('data-text', "new stuff");
+        
+        twttr.widgets.load()
+        ++counter;
 	});
 	
-    var displayQuote = document.getElementsByClassName('quo')[0].innerHTML;
-console.log(displayQuote);
-    $('#twitter-widget-' + counter).remove();
     
-    var newAnchorTag = $('<a></a>')
-        .addClass('twitter-share-button')
-        .attr('href', 'https://twitter.com/intent/tweet')        
-        .attr('data-text', displayQuote);
-    $('.oneCoolDiv').append(newAnchorTag);
-    //$(".twitter-share-button").attr('data-text', "new stuff");
-	
-	twttr.widgets.load()
-    ++counter;
-    console.log(counter);
+    
+    
 }
+
+
+
 
 document.getElementById("quoteButton").addEventListener("click", displayQuote);
 /*
